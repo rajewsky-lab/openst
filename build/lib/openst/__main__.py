0391252450cc51be32019cdcd185626c38c6dac4
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2023-09-08 13:15:48 +0200

    restructuring

--- build/lib/openst/__main__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from openst.cli import cmdline_main
-
-logging.basicConfig(format="%(levelname)s %(asctime)s - %(message)s", level=logging.INFO)
-
-if __name__ == "__main__":
-    cmdline_main()
