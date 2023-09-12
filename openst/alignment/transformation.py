bb8854bcee3f90da64d29ee63923d9e62feac7a3
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2023-09-12 15:45:55 +0200

    split transformation to a separate file

---
+++ openst/alignment/transformation.py
@@ -0,0 +1,27 @@
+import numpy as np
+from skimage.transform import SimilarityTransform
+
+
+def apply_transform(in_coords: np.ndarray, transform: SimilarityTransform, check_bounds=False):
+    # Check if transform within the acceptable bounds
+    if (
+        check_bounds
+        and (transform.rotation > np.pi / 4)
+        or (transform.scale > 2 or transform.scale < 0.5)
+        or (transform.translation.max() > in_coords.max(axis=0).max())
+    ):
+        return in_coords
+
+    # If the previous filter passes, apply the transformation
+    out_coords = np.dot(
+        transform.params,
+        np.concatenate(
+            [
+                in_coords[:, ::-1],
+                np.ones((len(in_coords), 1)),
+            ],
+            axis=1,
+        ).T,
+    ).T
+
+    return out_coords
