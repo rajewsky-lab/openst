81bfb2c234e9ec476186349af94159a307c091fb
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2023-09-14 11:09:36 +0200

    updating examples and debugging code

--- examples/metastatic_lymph_node/run_pipeline.sh
+++ examples/metastatic_lymph_node/run_pipeline.sh
@@ -29,21 +29,38 @@
 #     --savedir $OUTDIR/{} \
 #     --no_npy
 
-# Run pairwise alignment
+# # Run pairwise alignment
+# openst pairwise_aligner \
+#     --image-in /data/rajewsky/home/dleonpe/projects/openst_paper/data/1_images_ln/stitched/fc_sts_63_4/Image_Stitched_Composite.tif \
+#     --h5-in /data/rajewsky/home/dleonpe/projects/openst_paper/data/2_downstream/fc_sts_63/stitched/fc_sts_063_4_merged_stitched_spots.h5ad \
+#     --h5-out /data/rajewsky/home/dleonpe/projects/openst_paper/data/2_downstream/fc_sts_63/auto_aligned_he/fc_sts_063_4_stitched_spots_aligned.h5ad \
+#     --save-image-in-h5 \
+#     --feature-matcher 'LoFTR' \
+#     --ransac-coarse-max-trials 1000 \
+#     --ransac-fine-max-trials 1000 \
+#     --n-threads 8 \
+#     --ransac-coarse-residual-threshold 2 \
+#     --ransac-coarse-min-samples 2 \
+#     --metadata-out /data/rajewsky/home/dleonpe/projects/openst_paper/data/2_downstream/fc_sts_63/auto_aligned_he/fc_sts_063_4_alignment_metadata.pkl \
+#     --threshold-counts-coarse 2 \
+#     --fiducial-model /data/rajewsky/home/dleonpe/projects/openst_paper/repos/openst/models/yolov8s_fiducial_detection_universal.pt
+
+# # # Assign transcripts to cells using the segmentation mask. Needs metadata alignment
+# # openst assign_transcripts_to_mask
+
 openst pairwise_aligner \
-    --image-in /data/rajewsky/home/dleonpe/projects/openst_paper/data/1_images_ln/stitched/fc_sts_63_4/Image_Stitched_Composite.tif \
-    --h5-in /data/rajewsky/home/dleonpe/projects/openst_paper/data/2_downstream/fc_sts_63/stitched/fc_sts_063_4_merged_stitched_spots.h5ad \
-    --h5-out /data/rajewsky/home/dleonpe/projects/openst_paper/data/2_downstream/fc_sts_63/auto_aligned_he/fc_sts_063_4_stitched_spots_aligned.h5ad \
+    --image-in /data/rajewsky/home/dleonpe/projects/openst_paper/data/1_images_ln/stitched/fc_sts_63_7/Image_Stitched_Composite.tif \
+    --h5-in /data/rajewsky/home/dleonpe/projects/openst_paper/data/2_downstream/fc_sts_63/stitched/fc_sts_063_7_stitched_spots.h5ad \
+    --h5-out /data/rajewsky/home/dleonpe/projects/openst_paper/data/2_downstream/fc_sts_63/auto_aligned_he/fc_sts_063_7_stitched_spots_aligned.h5ad \
     --save-image-in-h5 \
-    --feature-matcher 'LoFTR' \
-    --ransac-coarse-max-trials 1000 \
-    --ransac-fine-max-trials 1000 \
+    --feature-matcher LoFTR \
+    --ransac-coarse-max-trials 50 \
+    --ransac-fine-max-trials 50 \
     --n-threads 8 \
     --ransac-coarse-residual-threshold 2 \
     --ransac-coarse-min-samples 2 \
-    --metadata-out /data/rajewsky/home/dleonpe/projects/openst_paper/data/2_downstream/fc_sts_63/auto_aligned_he/fc_sts_063_4_alignment_metadata.pkl \
-    --threshold-counts-coarse 2 \
-    --fiducial-model /data/rajewsky/home/dleonpe/projects/openst_paper/repos/openst/models/yolov8s_fiducial_detection_universal.pt
-
-# # Assign transcripts to cells using the segmentation mask. Needs metadata alignment
-# openst assign_transcripts_to_mask
+    --metadata-out /data/rajewsky/home/dleonpe/projects/openst_paper/data/2_downstream/fc_sts_63/auto_aligned_he/fc_sts_063_7_alignment_metadata.json \
+    --threshold-counts-coarse 5 \
+    --mask-tissue \
+    --keep-black-background \
+    --only-coarse
