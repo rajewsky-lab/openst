67138fee992744b2aa1678661167a7ae8ec3b02c
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2023-12-21 22:56:33 +0100

    added CUT preprocessing

---
+++ openst/preprocessing/CUT/README.md
@@ -0,0 +1,36 @@
+This code from the openst package has been adapted from the official Contrastive Unpaired Translation (CUT) implementation.
+
+We acknowledge the following publication
+```
+@inproceedings{park2020cut,
+  title={Contrastive Learning for Unpaired Image-to-Image Translation},
+  author={Taesung Park and Alexei A. Efros and Richard Zhang and Jun-Yan Zhu},
+  booktitle={European Conference on Computer Vision},
+  year={2020}
+}
+```
+
+## License
+Copyright (c) 2020, Taesung Park and Jun-Yan Zhu
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ openst/preprocessing/image_preprocess.py
@@ -0,0 +1,112 @@
+import argparse
+import numpy as np
+import os
+
+from PIL import Image
+from tqdm import tqdm
+
+# from openst.preprocessing.CUT.models import create_model
+
+def get_image_preprocess_parser():
+    """
+    Parse command-line arguments.
+
+    Returns:
+        argparse.Namespace: Parsed command-line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="preprocess imaging data with CUT model (as in open-ST paper)",
+        allow_abbrev=False,
+        add_help=False,
+    )
+    parser.add_argument('--input_img', type=str, required=True,
+                        help='path to input image')
+    parser.add_argument('--cut_dir', type=str, required=True,
+                        help='path to CUT directory (to save patched images)')
+    parser.add_argument('--tile_size_px', type=int, required=True,
+                        help='size of the tile in pixels')
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cpu",
+        choices=["cpu", "cuda"],
+        help="Device used to run feature matching model. Can be ['cpu', 'cuda']",
+    )
+    parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
+
+    return parser
+
+
+def setup_image_preprocess_parser(parent_parser):
+    """setup_image_preprocess_parser"""
+    parser = parent_parser.add_parser(
+        "image_preprocess",
+        help="convert fastq files into spatial barcode files (sequence and coordinates)",
+        parents=[get_image_preprocess_parser()],
+    )
+    parser.set_defaults(func=_run_image_preprocess)
+
+    return parser
+
+def _images_to_tiles(img):
+    _img_shape = img.shape
+    tiles = []
+    for x in range(0, _img_shape[0]-args.tile_size_px, args.tile_size_px):
+        for y in range(0, _img_shape[1]-args.tile_size_px, args.tile_size_px):
+            tiles.append([x, y])
+
+    imgs = []
+    for i, coord in tqdm(enumerate(tiles)):
+        imgs.append(img[coord[0]:(coord[0]+args.tile_size_px), coord[1]:(coord[1]+args.tile_size_px)])
+
+    return tiles, imgs 
+
+
+def _tiles_to_images(tiles, imgs, dest_shape):
+    img_restitch = np.zeros(dest_shape)
+    for i, coord in tqdm(enumerate(tiles)):
+        img_restitch[coord[0]:(coord[0]+args.tile_size_px), coord[1]:(coord[1]+args.tile_size_px)] = imgs[i] 
+
+
+def _image_preprocess(imgs_tiles, model):
+    for i, data in enumerate(dataset):
+        if i == 0:
+            model.data_dependent_initialize(data)
+            model.setup(opt)
+            model.parallelize()
+            if args.eval:
+                model.eval()
+        data = {'A': None, 'B': None, 'A_paths': None, 'B_paths': None}
+        model.set_input(data)  # unpack data from data loader
+        model.test()           # run inference
+        visuals = model.get_current_visuals()  # get image results
+
+
+def _run_image_preprocess():
+    # loading arguments
+    args = parser.parse_args()
+
+    Image.MAX_IMAGE_PIXELS = 933120000
+
+    # Load image and get shape
+    img = np.array(Image.open(args.input_img))
+    _img_shape = img.shape
+
+    tiles, imgs_tiles = _images_to_tiles(img)
+
+    opt = TestOptions().parse()
+    opt.num_threads = 1
+    opt.batch_size = 1
+    opt.serial_batches = True
+    opt.no_flip = True
+    opt.load_size = args.tile_size_px
+
+    model = create_model(opt)
+    imgs_tiles_processed = _image_preprocess(imgs_tiles)
+
+    img_restitch = _tiles_to_images(tiles, imgs_tiles_processed, _img_shape)
+    Image.fromarray(img_restitch.astype(np.uint8)).save(args.output_img)
+
+if __name__ == "__main__":
+    args = get_image_preprocess_parser().parse_args()
+    _run_image_preprocess(args)
