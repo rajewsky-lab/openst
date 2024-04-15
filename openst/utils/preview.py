258d48819f5d12f0698eeba628b18b9f192c8412
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2024-04-15 10:35:42 +0200

    added pseudoimage plotting & tree view to preview

--- openst/utils/preview.py
+++ openst/utils/preview.py
@@ -1,16 +1,16 @@
-def _run_preview(args):
-    import h5py
+import h5py
+import logging
+
+from openst.utils.pseudoimage import create_unpaired_pseudoimage
+from openst.utils.file import h5_to_dict
+
+def _show_viewer(adata, args):
     try:
         import napari
     except ImportError:
         raise ImportError(
             "Please install napari: `pip install napari`."
         )
-
-    from openst.utils.file import check_file_exists
-    
-    check_file_exists(args.h5_in)
-    adata = h5py.File(args.h5_in, 'r+')
 
     viewer = napari.Viewer()
 
@@ -23,6 +23,17 @@ def _run_preview(args):
 
             viewer.add_image(data=adata[_image_key][::image_resampling, ::image_resampling])
 
+    if args.pseudoimage_keys is not None:
+        for i, _pseudoimage_key in enumerate(args.pseudoimage_keys):
+            if len(args.pseudoimage_units_to_um) != len(args.pseudoimage_keys):
+                pseudoimage_units_to_um = args.pseudoimage_units_to_um[0]
+            else:
+                pseudoimage_units_to_um = args.pseudoimage_units_to_um[i]
+
+            _pseudoimage, _ = create_unpaired_pseudoimage(adata, _pseudoimage_key, pseudoimage_units_to_um, write_rescaled=False)
+
+            viewer.add_image(data=_pseudoimage)
+
     if args.spatial_coord_keys is not None:
         for i, _spatial_coord_key in enumerate(args.spatial_coord_keys):
             if len(args.spatial_coord_resampling) != len(args.spatial_coord_keys):
@@ -33,6 +44,27 @@ def _run_preview(args):
             viewer.add_points(data=adata[_spatial_coord_key][::spatial_coord_resampling])
     
     napari.run()
+
+def _show_tree(adata, args):
+    import json
+
+    adata_tree = h5_to_dict(adata)
+    d = json.dumps(adata_tree, indent=4)
+    print(d)
+    
+
+def _run_preview(args):
+    from openst.utils.file import check_file_exists
+    
+    check_file_exists(args.h5_in)
+    adata = h5py.File(args.h5_in, 'r+')
+
+    if args.file_structure:
+        logging.info(f"Showing structure from {args.h5_in}")
+        _show_tree(adata, args)
+    else:
+        logging.info(f"Opening napari for {args.h5_in}")
+        _show_viewer(adata, args)
 
 if __name__ == "__main__":
     from openst.cli import get_preview_parser
